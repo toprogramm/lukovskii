d5c30f757070ac46d9b7c219632812e88b3cc8d6
toprogramm <toprogramm@gmail.com>
2023-02-11 15:23:04 +0600

    added credits to progect

---
+++ src/modules/Credits/Credits.tsx
@@ -0,0 +1,26 @@
+const credits = {
+  transparentBackground: (
+    <>
+      Free SVG Background by{" "}
+      <a target="_blank" href="https://bgjar.com">
+        BGJar
+      </a>
+    </>
+  ),
+  icon: (
+    <>
+      <a href="https://www.freepik.com/free-vector/vector-green-leaf-icons-white-eco-concept_10601443.htm#query=leaf&position=1&from_view=search&track=sph">
+        Image by macrovector
+      </a>{" "}
+      on Freepik
+    </>
+  ),
+  photoBackground: (
+    <>
+      <a href="https://www.freepik.com/free-vector/abstract-bokeh-blurred-lights-wallpaper_2582498.htm#page=5&query=green%20wallpaper&position=5&from_view=search&track=sph">
+        Image by rawpixel.com
+      </a>{" "}
+      on Freepik
+    </>
+  ),
+};
