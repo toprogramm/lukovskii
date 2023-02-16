9da51efdf1973adb9634dd52741bb7ea4e1f3b78
toprogramm <toprogramm@gmail.com>
2023-02-17 03:25:01 +0600

    added scrolling

--- src/modules/Navigation/Navigation.tsx
+++ src/modules/Navigation/Navigation.tsx
@@ -2,28 +2,40 @@ import "./Navigation.scss";
 import Button from "../Button/Button";
 import { ColorVariant } from "../Button/Button";
 import { ShadowVariant } from "../Button/Button";
+import { ScrollCenter, ScrollTop } from "../Scroll/Scroll";
 
 const Navigation = () => {
   return (
     <div className="Navigation">
-      <Button
-        colorVariant={ColorVariant.usual}
-        shadowVariant={ShadowVariant.none}
-      >
-        <h5>About me</h5>
-      </Button>
-      <Button
-        colorVariant={ColorVariant.usual}
-        shadowVariant={ShadowVariant.none}
-      >
-        <h5>Expirience</h5>
-      </Button>
-      <Button
-        colorVariant={ColorVariant.usual}
-        shadowVariant={ShadowVariant.none}
-      >
-        <h5>Services</h5>
-      </Button>
+      <div className="Navigation__box">
+        <Button
+          onClick={() => {
+            ScrollCenter("AboutMe");
+          }}
+          colorVariant={ColorVariant.usual}
+          shadowVariant={ShadowVariant.none}
+        >
+          <h5>About me</h5>
+        </Button>
+        <Button
+          onClick={() => {
+            ScrollTop("Experience");
+          }}
+          colorVariant={ColorVariant.usual}
+          shadowVariant={ShadowVariant.none}
+        >
+          <h5>Experience</h5>
+        </Button>
+        <Button
+          onClick={() => {
+            ScrollCenter("Contacts");
+          }}
+          colorVariant={ColorVariant.usual}
+          shadowVariant={ShadowVariant.none}
+        >
+          <h5>Services</h5>
+        </Button>
+      </div>
     </div>
   );
 };
